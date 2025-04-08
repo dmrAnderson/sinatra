bad2d5b6a0e118a6acc6583e6b773e32caf9f7ca
Artem Skrynnyk <skrynnyk.artem@coaxsoft.com>
2025-04-09 00:33:06 +0300

    Current user

--- app.rb
+++ app.rb
@@ -6,8 +6,29 @@ require 'sequel'
 set :environment, ENV.fetch('RACK_ENV', 'development')
 set :database_url, ENV.fetch('DATABASE_URL', 'postgres://postgres:postgres@localhost:5432/sinatra_app')
 set :database, Sequel.connect(settings.database_url)
+set :session_secret, ENV.fetch('SUPER_SECRET_KEY', settings.database_url * 2)
 
 require './models/user'
+
+enable :sessions
+
+helpers do
+  def current_user
+    @current_user ||= User[session[:user_id]] if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def logout
+    session[:user_id] = nil
+  end
+end
 
 get '/' do
   'Hello from Sinatra'
@@ -18,9 +39,26 @@ post '/signup' do
   user.password = params[:password]
   if user.valid?
     user.save
+    login(user)
     "User created"
   else
     status 422
     "Signup failed"
   end
+end
+
+post '/login' do
+  user = User.where(email: params[:email]).first
+  if user && user.valid_password?(params[:password])
+    login(user)
+    redirect '/'
+  else
+    status 401
+    "Invalid credentials"
+  end
+end
+
+delete '/logout' do
+  logout
+  redirect '/login'
 end
--- test/app_test.rb
+++ test/app_test.rb
@@ -21,7 +21,7 @@ class AppTest < Minitest::Test
     assert_equal 'text/html;charset=utf-8', last_response.content_type
   end
 
-  def test_successful_signup
+  def test_authentication
     User.dataset.delete
 
     post '/signup', email: 'test@example.com', password: 'secret123'
@@ -32,15 +32,28 @@ class AppTest < Minitest::Test
     user = User.first
     assert_equal 'test@example.com', user.email
     assert user.valid_password?('secret123')
-  end
+    assert_equal user.id, last_request.env['rack.session'][:user_id]
 
-  def test_signup_with_missing_email
     post '/signup', email: 'test@example.com', password: 'secret123'
 
     assert_equal 422, last_response.status
     assert_equal 'Signup failed', last_response.body
-    assert_equal 1, User.count
 
-    User.dataset.delete
+    delete '/logout'
+
+    assert_equal 302, last_response.status
+    assert_equal 'http://example.org/login', last_response.location
+    assert_nil last_request.env['rack.session'][:user_id]
+
+    post '/login'
+
+    assert_equal 401, last_response.status
+    assert_equal 'Invalid credentials', last_response.body
+
+    post '/login', email: 'test@example.com', password: 'secret123'
+
+    assert_equal 302, last_response.status
+    assert_equal 'http://example.org/', last_response.location
+    assert_equal user.id, last_request.env['rack.session'][:user_id]
   end
 end
